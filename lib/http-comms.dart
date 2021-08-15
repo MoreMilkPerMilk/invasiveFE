9db9fcde1e2a31fa9f710141aeea69dd587ef674
Hamish Bultitude <sendanemailtohamish@gmail.com>
2021-08-15 22:16:52 +1000

    GET all /users

--- lib/http-comms.dart
+++ lib/http-comms.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'dart:developer';
+
+import 'package:invasive_fe/models.dart';
+
+var API_URL = 'http://127.0.0.1:8000';
 
 class HttpTestPage extends StatelessWidget {
   @override
@@ -12,10 +20,32 @@ class HttpTestPage extends StatelessWidget {
         child: ElevatedButton(
           onPressed: () {
             log("Hello!");
+            getAllLocations();
           },
-          child: Text('Test hello!'),
+          child: Text('Test /person!'),
         ),
       ),
     );
+  }
+}
+
+
+/// communicate with backend server to HTTP GET all weed instances.
+Future<List<User>> getAllLocations() async {
+  final response = await http.get(Uri.parse(API_URL + "/users"));
+
+  if (response.statusCode == 200) {
+    log(response.body);
+    // var result = await compute(User.parseUserList, response.body);
+    var result = User.parseUserList(response.body);
+    result.forEach((element) {
+      print(element);
+    });
+
+    // return result;
+    return [];
+    // return compute(WeedInstance.parseWeedInstanceList, response.body);
+  } else {
+    return [];
   }
 }
