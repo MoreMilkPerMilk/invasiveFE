618f34498963aaf1a1bb12fb787a94876cfaae71
James Tobler <jamesjtobler@gmail.com>
2021-08-16 13:08:41 +1000

    added a flutter_maps map to the map page, modified map tile style

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+// openstreetmap tile servers: https://wiki.openstreetmap.org/wiki/Tile_servers
+
+const String MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoianNsdm4iLCJhIjoiY2tzZTFoYmltMHc5ajJucX'
+    'RiczY3eno3bSJ9.We8R6YRT_fcmuC6bOzzqbw';
+
 
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var title = Text("Maps Page");
-    var appBar = AppBar(title: title);
-    var body = Center();
-    var scaffold = Scaffold(appBar: appBar, body: body);
-    return scaffold;
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Maps Page")),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(-27.4975, 153.0137),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+              urlTemplate: "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b']
+          ),
+          MarkerLayerOptions(
+            markers: [],
+          ),
+        ],
+      )
+    );
   }
 }
 
