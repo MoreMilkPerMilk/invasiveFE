88af5d3d7bcb6f830b8079a6caf5fb9c76c2ee70
James Tobler <jamesjtobler@gmail.com>
2021-08-23 23:44:46 +1000

    added architecture documentation. WARNING: code does not work at this point!

--- lib/maps.dart
+++ lib/maps.dart
@@ -4,6 +4,51 @@ import 'package:latlong2/latlong.dart';
 
 // openstreetmap tile servers: https://wiki.openstreetmap.org/wiki/Tile_servers
 
+/**
+ * Architecture
+ *
+ * The main page consists of a scaffold, which fills with a flutter_map.
+ * This map contains a series of markers. Markers are containers for widgets,
+ * with some extra meta-data like geo-location which enables their placement on
+ * the map, in the form of the widgets they contain.
+ *
+ * The widgets these markers contain are WeedMarkers. WeedMarkers are stateful
+ * widgets which can be active (i.e. clicked on and displaying information) or
+ * inactive. Each WeedMarker contains marker-specific information such as the
+ * weed name, and other data. They are built as IconButtons, though this can
+ * easily be modified to any clickable widget (i.e. using GestureDetector).
+ *
+ * Each WeedMarker also contains a reference to a shared OverlayEntry. This
+ * OverlayEntry is a container for a widget (the visual overlay) that is
+ * displayed to the screen when inserted by the OverlayState. All of this is
+ * nicely packaged into the WeedOverlay class, which handles
+ *
+ * Each WeedMarker also contains a reference a shared WeedOverlay class, which
+ * represents the pop-up for selecting a marker. This class bundles together
+ * information about whether an overlay is showing, a reference to the
+ * OverlayState object and OverlayEntry object which are both necessary for
+ * displaying and modifying the overlay, as well as the widget that the
+ * OverlayEntry builds upon its display.
+ *
+ * The process of clicking a weed marker is as such:
+ * The IconButton representing the WeedMarker is clicked.
+ * The marker becomes active, is rebuilt, and may change appearance.
+ * It alerts the WeedOverlay that is has been clicked, and provides its
+ * weed information.
+ * The WeedOverlay updates the widget contained in the OverlayEntry to contain
+ * the marker's information. It then rebuilds the OverlayEntry so that these
+ * changes are shown visually.
+ * If the overlay was not already displaying, the WeedOverlay calls upon its
+ * OverlayState to insert the OverlayEntry into the context, thus displaying it.
+ * The overlay is now displayed with the marker's information on it.
+ *
+ * This architecture is extremely flexible, with one trade-off: you
+ * can't have multiple overlays at once, since only one shared overlay entry is
+ * used. If you want this functionality, you'll need to create a new overlay
+ * entry for each WeedMarker, then pass this entry into the WeedOverlay to be
+ * inserted by its OverlayState.
+ */
+
 
 /// fixme; James's big bug bash board:
 /// Clicking a marker multiple times generates multiple overlays.
@@ -29,6 +74,8 @@ class MapsPage extends StatelessWidget {
           options: MapOptions(
             center: LatLng(-27.4975, 153.0137),
             zoom: 13.0,
+            // enable pinchZoom and drag (move) only
+            interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
           ),
           layers: [
             TileLayerOptions(
@@ -47,11 +94,14 @@ class MapsPage extends StatelessWidget {
   }
 
   Marker generateMarker(String weedName, LatLng point) {
+
     return Marker(
             width: 40,
             height: 40,
             point: point,
             builder: (context) =>
+                WeedMarker()
+            /*
                 Container(
                   child: IconButton(
                     icon: Icon(
@@ -62,6 +112,8 @@ class MapsPage extends StatelessWidget {
                       print('Marker pressed!');
                       var overlayState = Overlay.of(context);
                       var overlayEntry;
+
+
                       overlayEntry = OverlayEntry(
                           builder: (context) =>
                               Center(
@@ -76,12 +128,10 @@ class MapsPage extends StatelessWidget {
                                                 Text(weedName),
                                                 IconButton(
                                                     onPressed: () {
-                                                      overlayEntry
-                                                          .remove();
+                                                      overlayEntry.remove();
                                                     },
-                                                    icon: Icon(
-                                                        Icons.close
-                                                    ))
+                                                    icon: Icon(Icons.close)
+                                                )
                                               ]
                                           )
                                       )
@@ -89,10 +139,50 @@ class MapsPage extends StatelessWidget {
                       overlayState!.insert(overlayEntry);
                     },
                   )
-              )
+              ) */
     );
   }
 }
 
 
+class WeedMarker extends StatefulWidget {
+  final String weedName;
+  // final
 
+  const WeedMarker(this.weedName);
+
+  @override
+  _WeedMarkerState createState() => _WeedMarkerState();
+}
+
+class _WeedMarkerState extends State<WeedMarker> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+      print("toggled active");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("rebuilding...");
+    return Container(
+        child: IconButton(
+          icon: Icon(
+              _active ? Icons.location_pin : Icons.airplane_ticket,
+              size: 40
+          ),
+          onPressed: () {
+            _handleTap();
+          },
+        )
+    );
+  }
+}
+
+
+class WeedOverlay extends StatefulWidget {
+
+}
