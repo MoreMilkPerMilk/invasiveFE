db96fbdb3c8fe3d2bc64faf3b95d15852080f15a
JoeHolownia <joeholownia7@gmail.com>
2021-09-21 22:25:30 +1000

    Made it actually build properly, added placeholder image for http test (having issue with xfile path atm)

--- lib/models/PhotoLocation.dart
+++ lib/models/PhotoLocation.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:objectid/objectid.dart';
 import 'package:geojson/geojson.dart';
 import 'package:geopoint/geopoint.dart';
@@ -10,36 +11,27 @@ import 'WeedInstance.dart';
 
 class PhotoLocation {
   ObjectId id;
-  String name;
-  Image photo;
+  XFile photo;
   GeoPoint location;
   List<WeedInstance> weeds_present;
 
   PhotoLocation({
     required this.id,
-    required this.name,
     required this.photo,
     required this.location,
-    required this.weeds_present,
+    required this.weeds_present
   });
 
-  String toJson() {
 
-    // get weeds json data
-    List<String> weedsPresentJson = [];
-    weeds_present.forEach((element) {
-      weedsPresentJson.add(element.toJson());
-    });
+  String toJson() { // may be redundant with api call structure
 
     return jsonEncode(<String, dynamic>{
       '_id': id.toString(),
-      'name': name,
       'point': GeoJsonPoint(geoPoint: location),
-      'weeds_present': weedsPresentJson,
     });
   }
 
-  factory PhotoLocation.fromJson(Map<String, dynamic> json, Image photo) {
+  factory PhotoLocation.fromJson(Map<String, dynamic> json, XFile photo) {
     List<WeedInstance> weeds_present = [];
     json['weeds_present'].forEach((element) {
       weeds_present.add(WeedInstance.fromJson(element));
@@ -47,7 +39,6 @@ class PhotoLocation {
 
     return PhotoLocation(
       id: ObjectId.fromHexString(json['_id']),
-      name: json['name'],
       photo: photo,
       location: json['location'],
       weeds_present: weeds_present,
@@ -58,23 +49,16 @@ class PhotoLocation {
   String toString() {
     var output = "";
     output += "_id: ${this.id}\n";
-    output += "name: ${this.name}\n";
     output += "point: ${this.location.toString()}\n";
-    output += "weeds_present:\n";
-    int i = 0;
-    weeds_present.forEach((element) {
-      output += "$i:";
-      output += "\t $element";
-      i++;
-    });
-
     return output;
   }
 
-
-  /// Parse a list of Locations in JSON format
-  static List<PhotoLocation> parsePhotoLocationList(String responseBody) {
+  /// Parse a list of Locations in JSON format todo: refactor this
+  static Future<List<PhotoLocation>> parsePhotoLocationList(String responseBody) async {
+    ByteData imgBytes = await rootBundle.load('assets/placeholder.png');
+    Uint8List imgUint8List = imgBytes.buffer.asUint8List(imgBytes.offsetInBytes, imgBytes.lengthInBytes);
+    XFile xFile = XFile.fromData(imgUint8List);
     final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-    return parsed.map<PhotoLocation>((json) => PhotoLocation.fromJson(json, img)).toList();
+    return parsed.map<PhotoLocation>((json) => PhotoLocation.fromJson(json, xFile)).toList();
   }
 }
