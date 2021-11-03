defb9bb3f9b48ecc3132bd5947b7529e580e05aa
Djamahl <djamahletchegaray@gmail.com>
2021-11-04 07:34:51 +1000

    fix slide up panel, add bad inference details and manual report button, tidy

--- lib/widgets/cameraHome.dart
+++ lib/widgets/cameraHome.dart
@@ -7,12 +7,12 @@ import 'package:invasive_fe/services/classifier_float.dart';
 import 'package:invasive_fe/services/classifier_scraped.dart';
 import 'package:invasive_fe/widgets/panel.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:string_extensions/string_extensions.dart';
 import 'package:tflite/tflite.dart';
 import 'dart:math' as math;
 import 'dart:io';
 import 'package:image/image.dart' as img;
 
-import '../services/camera.dart';
 import 'bndbox.dart';
 import '../services/models.dart';
 
@@ -61,11 +61,8 @@ class _CameraHomePageState extends State<CameraHomePage> {
   @override
   void initState() {
     super.initState();
-    // loadCameras();
-    // Future cameraFuture = availableCameras();
-    // cameraFuture.then((value) => cameras = value);
-    // onSelect(resnet);
-    // loaded = Future.wait([cameraFuture]);
+    //wait on nothing by default
+    loaded = Future.wait([]);
   }
 
   Future getImagefromCamera() async {
@@ -81,11 +78,11 @@ class _CameraHomePageState extends State<CameraHomePage> {
 
       img.Image imageInput = img.decodeImage(File(path).readAsBytesSync())!;
       var pred = deepWeedsClassifier.predict(imageInput);
-      label = pred.label + " " + pred.score.toString();
+      label = pred.label.toTitleCase()! + " " + pred.score.toStringAsFixed(3);
 
       goodDetection = false;
       if (pred.score > THRESHOLD) {
-        foundSpecies = pred.label;
+        foundSpecies = pred.label.toTitleCase()!;
         goodDetection = true;
       }
       print("PREDICTION-----S");
@@ -103,86 +100,263 @@ class _CameraHomePageState extends State<CameraHomePage> {
       topRight: Radius.circular(24.0),
     );
 
+    //76,174,80
     return Scaffold(
-        body: GestureDetector(
-            // onTap: getImagefromCamera,,
-            child: Container(
-              // color: ,
-              child: SlidingUpPanel(
-                backdropEnabled: true,
-                controller: _pc,
-                minHeight: 0,
-                // maxHeight: foundSpecies != "Negatives" ? 500 : 300,
-                maxHeight: !goodDetection ? 300 : 500,
-                // panel: Panel(foundSpecies, photo, _pc, foundSpecies
-                // == "Negatives"),
-                panel: Panel(foundSpecies, photo, _pc, !goodDetection),
-                body: SafeArea(
-                  minimum: const EdgeInsets.only(top: 30),
-                  child: Column(
-                    children: [
-                      //title
-                      Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                              'Classify',
-                              style: TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold)
-                          )
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text('Open the camera to classify an invasive species.')
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Align(
-                            alignment: Alignment.topRight,
-                            child: OutlinedButton.icon(
-                              style: TextButton.styleFrom(
-                                primary: Colors.green,
-                                // shape:
+        body: FutureBuilder(
+            future: loaded,
+            builder: (context, snapshot) {
+              // if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                  color: Color.fromRGBO(76, 174, 80, 1),
+                  child: SlidingUpPanel(
+                    backdropEnabled: true,
+                    controller: _pc,
+                    minHeight: 0,
+                    // maxHeight: foundSpecies != "Negatives" ? 500 : 300,
+                    maxHeight: !goodDetection ? 300 : 500,
+                    // panel: Panel(foundSpecies, photo, _pc, foundSpecies
+                    // == "Negatives"),
+                    panel: Panel(foundSpecies, photo, _pc, !goodDetection),
+                    body: (snapshot.connectionState == ConnectionState.done) ? SafeArea(
+                        minimum: const EdgeInsets.only(top: 30),
+                        child: Column(
+                          children: [
+                            Container(
+                              // color: Colors.green,
+                              // margin: const EdgeInsets.all(0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Text('Classify',
+                                          style: TextStyle(
+                                              fontSize: 25,
+                                              // color: Color.fromRGBO(76, 174, 80, 1),
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold))),
+                                  Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Text(
+                                          'Open the camera to classify an invasive species.',
+                                      style: TextStyle(color: Colors.white),)),
+                                ],
                               ),
-                              onPressed: () {
-                                _pc.close();
-                                getImagefromCamera();
-                              },
-                              icon: Icon(Icons.camera_alt_outlined, size: 18),
-                              label: Text("Open Camera"),
-                            )
-                        )
-                      ),
-                      if (path != "")
-                        Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Column(
-                              children: [
-                                Image.file(
-                                  File(path),
-                                  width: 300,
+                            ),
+                            Expanded(
+                              child: Container(
+                                //217, 222, 175
+                                //238, 240, 221
+                                // color: Color.fromRGBO(218, 222, 182, 1),
+                                color: Color.fromRGBO(238, 240, 221, 1.0),
+                              margin: const EdgeInsets.only(bottom: 0),
+                                padding: const EdgeInsets.only(top: 20),
+                                child: Column(
+                                  children: [
+
+                                    Card(
+                                      //151, 199, 247
+                                      color: Color.fromRGBO(74, 95, 145, 1),
+                                      margin: const EdgeInsets.only(
+                                          left: 10, right: 10),
+                                      child: Column(
+                                        children: [
+                                          Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 20,
+                                                  bottom: 20,
+                                                  left: 50,
+                                                  right: 50),
+                                              // child: Image(
+                                              //     image: path == "" ? AssetImage('assets/capture_example.gif') : File(path),
+                                              //     width: 300,
+                                              //     height: 300)
+                                              child: (path == "")
+                                                  ? Image(
+                                                      image: AssetImage(
+                                                          'assets/capture_example.gif'),
+                                                      width: 250,
+                                                      height: 250)
+                                                  : Image.file(
+                                                      File(path),
+                                                      width: 200,
+                                                    )),
+                                          Padding(
+                                              padding: const EdgeInsets.only(bottom:5, left:15, right:15),
+                                              child: Text(
+                                                  (path == "") ? "Try and frame the plants flowers or leaves as the subject of the capture. " : label,
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                              textAlign: TextAlign.center,)),
+                                          if (!goodDetection && path != "")
+                                            Card(
+                                              color: Color.fromRGBO(220, 220, 220, 1),
+                                              margin: const EdgeInsets.all(10),
+                                              child: Column(
+                                                children: [
+                                                  Padding(
+                                                    padding: const EdgeInsets.all(8),
+                                                    child: Text("The model wasn't confident it detected an invasive species. You can manually report it if you are sure "
+                                                        "the captured species is a weed.", textAlign: TextAlign.center, style: TextStyle(color: Colors.black))
+                                                  ),
+                                                  Padding(
+                                                    padding: EdgeInsets.all(10),
+                                                    child: OutlinedButton.icon(
+                                                      style: TextButton.styleFrom(
+                                                        primary: Colors.red,
+                                                        backgroundColor: Colors.white,
+                                                        // shape:
+                                                      ),
+                                                      onPressed: () {
+                                                        setState(() {
+                                                          // widget.buttonColor = Colors.grey;
+
+                                                        });
+                                                      },
+                                                      icon: Icon(Icons.send, size: 18),
+                                                      label: Text("REPORT"),
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.all(10),
+                                        child: Align(
+                                            alignment: Alignment.center,
+                                            child: OutlinedButton.icon(
+                                              style: TextButton.styleFrom(
+                                                primary: Colors.green,
+                                                backgroundColor: Colors.white,
+                                                // shape:
+                                              ),
+                                              onPressed: () {
+                                                setState(() {
+                                                  _pc.close();
+                                                  loaded = getImagefromCamera();
+                                                });
+                                                // getImagefromCamera();
+                                              },
+                                              icon: Icon(
+                                                  Icons.camera_alt_outlined,
+                                                  size: 18),
+                                              label: Text("CAPTURE"),
+                                            ))),
+                                  ],
                                 ),
-                                Text(label)
-                              ],
+                              ),
                             )
-                        )
-                      else
-                        Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Image(
-                            image: AssetImage('assets/capture_example.gif'),
+                          ],
+                        )) :
+                    Align(
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator()),
+                    borderRadius: radius,
+                    // onPanelClosed: () {
+                    //   Fluttertoast.cancel(); // hide all toasts
+                    //   repeatedNegativeCount = 0;
+                    //   repeatedNegative = false;
+                    //   startTfliteDetection();
+                    // },
+                  ),
+                );
+              // } else {
+              //   // the futures have not yet completed; display a loading page
+              //   return Container(
+              //     // color: ,
+              //     child: SlidingUpPanel(
+              //       backdropEnabled: true,
+              //       controller: _pc,
+              //       minHeight: 0,
+              //       // maxHeight: foundSpecies != "Negatives" ? 500 : 300,
+              //       maxHeight: !goodDetection ? 300 : 500,
+              //       // panel: Panel(foundSpecies, photo, _pc, foundSpecies
+              //       // == "Negatives"),
+              //       panel: Panel(foundSpecies, photo, _pc, !goodDetection),
+              //       body: Align(
+              //           alignment: Alignment.center,
+              //           child: CircularProgressIndicator()),
+              //       borderRadius: radius,
+              //     ),
+              //   );
+              // }
+            }));
+
+    return Scaffold(
+        body: Container(
+      // color: ,
+      child: SlidingUpPanel(
+        backdropEnabled: true,
+        controller: _pc,
+        minHeight: 0,
+        // maxHeight: foundSpecies != "Negatives" ? 500 : 300,
+        maxHeight: !goodDetection ? 300 : 500,
+        // panel: Panel(foundSpecies, photo, _pc, foundSpecies
+        // == "Negatives"),
+        panel: Panel(foundSpecies, photo, _pc, !goodDetection),
+        body: SafeArea(
+            minimum: const EdgeInsets.only(top: 30),
+            child: Column(
+              children: [
+                //title
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text('Classify',
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold))),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                        'Open the camera to classify an invasive species.')),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                        alignment: Alignment.topRight,
+                        child: OutlinedButton.icon(
+                          style: TextButton.styleFrom(
+                            primary: Colors.green,
+                            // shape:
+                          ),
+                          onPressed: () {
+                            _pc.close();
+                            getImagefromCamera();
+                          },
+                          icon: Icon(Icons.camera_alt_outlined, size: 18),
+                          label: Text("Open Camera"),
+                        ))),
+                if (path != "")
+                  Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Column(
+                        children: [
+                          Image.file(
+                            File(path),
                             width: 300,
-                            height: 300)
-                        )
-                    ],
-                  )
-                ),
-                borderRadius: radius,
-                // onPanelClosed: () {
-                //   Fluttertoast.cancel(); // hide all toasts
-                //   repeatedNegativeCount = 0;
-                //   repeatedNegative = false;
-                //   startTfliteDetection();
-                // },
-              ),
-            )));
+                          ),
+                          Text(label)
+                        ],
+                      ))
+                else
+                  Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Image(
+                          image: AssetImage('assets/capture_example.gif'),
+                          width: 300,
+                          height: 300))
+              ],
+            )),
+        borderRadius: radius,
+        // onPanelClosed: () {
+        //   Fluttertoast.cancel(); // hide all toasts
+        //   repeatedNegativeCount = 0;
+        //   repeatedNegative = false;
+        //   startTfliteDetection();
+        // },
+      ),
+    ));
   }
 }
